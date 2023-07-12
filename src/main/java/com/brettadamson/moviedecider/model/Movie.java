52f8adb174c2b6a306bb9b9ea0042460c3fc24d9
BrettAdamson <brettmadamson@gmail.com>
2023-07-11 22:43:44 -0500

    working on getting data persisted

---
+++ src/main/java/com/brettadamson/moviedecider/model/Movie.java
@@ -0,0 +1,15 @@
+package com.brettadamson.moviedecider.model;
+
+public class Movie {
+
+    private String title;
+    public Movie(){
+
+    }
+
+    public Movie(String title){
+        this.title = title;
+    }
+
+
+}
---
+++ src/main/java/com/brettadamson/moviedecider/services/UserService.java
@@ -0,0 +1,14 @@
+package com.brettadamson.moviedecider.services;
+
+
+import com.brettadamson.moviedecider.repositories.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private UserRepository userRepository;
+
+
+
+}
